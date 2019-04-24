a7b03c6696f85fcec179af69912e0e6fb970e03a
waninko <43968374+waninko@users.noreply.github.com>
2019-04-24 14:12:07 +0200

    Filtering the Booklist

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+
+    <hr>
+    <h2>Filtered Books By Ownership </h2>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{filter}}</option>
+    </select>
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
@@ -10,6 +16,7 @@
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -20,19 +27,26 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true },
-        { title: "American Gods", author: "Neil Gaiman", finishedReading: false },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true }
-      ]
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true, ownersihp: "borrowed" },
+        { title: "American Gods", author: "Neil Gaiman", finishedReading: false, ownersihp: "bought" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true,  ownersihp: "borrowed" }
+      ],
+      filters: ["bought", "borrowed"],
+      holding: "bought"
     };
   },
   components: {
     BookItem,
     BookForm
   },
+  computed:{
+    filteredBooks() {
+    return _.filter(this.books, ["ownership", this.holding]);
+}
+  },
   methods: {
     appendBook(bookData) {
-      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading: bookData.finishedReading });
+      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading: bookData.finishedReading, ownersihp: bookData.ownersihp });
     }
   }
 };
